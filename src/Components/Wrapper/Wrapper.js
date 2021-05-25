9fdc09df1f3d06bf83f19d7a406bd748ca2221a2
Ross <ross.lillis@gmail.com>
2021-05-25 13:46:37 +1000

    feat(Wrapper): create wrapper component for main body

---
+++ src/Components/Wrapper/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Wrapper.css';
+
+const Wrapper = () => {
+  return (
+    <div>
+      <section>Wrapper</section>
+    </div>
+  )
+}
+
+export default Wrapper
