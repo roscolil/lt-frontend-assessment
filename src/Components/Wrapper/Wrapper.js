a098561834b61872856779ad2c8e88537dcc1e57
Ross <ross.lillis@gmail.com>
2021-05-27 12:31:45 +1000

    feat(components/wrapper): build additional wrapper for button styles

--- src/Components/Wrapper/Wrapper.js
+++ src/Components/Wrapper/Wrapper.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import './Wrapper.css';
+import './Wrapper.scss';
+import ClassicLink from '../ClassicLink/ClassicLink';
+import ShowsLink from '../ShowsLink/ShowsLink';
+import MusicLink from '../MusicLink/MusicLink';
+import data from '../../data/data.json';
 
+// TODO loop through how ever many links are present in the response and display button for each
 const Wrapper = () => {
   return (
-    <div>
-      <section>Wrapper</section>
+    <div className="wrapper">
+      <ClassicLink text="Classic link" href="https://linktr.ee" />
+      <ShowsLink title="Shows link" data={data[0].shows} />
+      <MusicLink title="Music link" data={data[0].music} />
     </div>
   )
 }
-
+// TODO these could be combined into one single link component with props
 export default Wrapper
