26d284637aa2202d34c11466f2ee63f8030382cd
Ross <ross.lillis@gmail.com>
2021-05-27 12:33:59 +1000

    feat(components/dropdown): build dropdown component

---
+++ src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './Dropdown.scss'
+import chevron from '../../assets/chevron-right.png'
+// TODO only toggle one link at a time
+const Dropdown = ({ title, items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  // TODO click through to display details of show/venue on click
+  function handleOnClick() { alert('You need to pay to access this feature! :)') }
+
+  return (
+    <div>
+      <div className="dropdown-btn"
+        onClick={() => toggle(!isOpen)}
+      >
+        {title}
+      </div>
+      <div className={`dropdown ${isOpen ? 'open' : 'close'}`}>
+        {isOpen && (
+          <ul className="dropdown-wrapper">
+            {
+              items.map((item, index) => (
+                <li li className="dropdown-li" key={index} >
+                  <button className="li-btn" onClick={() => handleOnClick()}>
+                    <div className="li-venue-wrapper">
+                      <span className="li-date">{item.date}</span>
+                      <span className="li-venue">{item.venue}</span>
+                    </div>
+                    <span
+                      className="li-tickets"
+                    >
+                      {
+                        item.tickets ?
+                          <img
+                            className="li-image"
+                            src={chevron}
+                            alt="" /> :
+                          'Sold out'
+                      }
+                    </span>
+                  </button>
+                </li>
+              ))
+            }
+            <li className="dropdown-footer">
+              <a href="https://www.songkick.com/">
+                <span>by Songkick</span>
+              </a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Dropdown
